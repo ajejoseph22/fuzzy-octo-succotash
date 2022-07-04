69dc394921d606b6db98776ff207604288e8f8a3
Joseph Aje <ajejoseph22@gmail.com>
2022-07-04 23:05:04 +0100

    chore: completed network + infra

--- cfn-helpers/deploy.sh
+++ cfn-helpers/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+#TMPL=$(cfn-include template.yml -y)
+#sed s/T00:00:00.000Z//g <<< "$TMPL" > combined.yml
+#aws cloudformation create-stack --stack-name "$1" --template-body file://combined.yml --parameters file://"$2" --capabilities CAPABILITY_NAMED_IAM
+
 aws cloudformation create-stack --stack-name "$1" --template-body file://"$2" --parameters file://"$3" --capabilities CAPABILITY_NAMED_IAM
